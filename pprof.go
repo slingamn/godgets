483dd59f7d5f50006c3bb199907b5354d053abb6
Shivaram Lingamneni <slingamn@cs.stanford.edu>
2021-05-12 00:08:32 -0400

    add StartPprofListener

---
+++ pprof.go
@@ -0,0 +1,17 @@
+// Copyright (c) 2021 Shivaram Lingamneni
+// released under the MIT license
+
+package godgets
+
+import (
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+)
+
+// be careful that the DefaultServeMux is not exposed anywhere else:
+func StartPprofListener(address string) {
+	go func() {
+		log.Println(http.ListenAndServe(address, nil))
+	}()
+}
