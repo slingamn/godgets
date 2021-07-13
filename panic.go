edbb72b01a68bedb70f1c5600acf1f6a2e821767
Shivaram Lingamneni <slingamn@cs.stanford.edu>
2021-07-13 14:02:08 -0400

    add generic panic handler

---
+++ panic.go
@@ -0,0 +1,24 @@
+// Copyright (c) 2021 Shivaram Lingamneni
+// released under the MIT license
+
+package godgets
+
+import (
+	"log"
+	"time"
+	"runtime/debug"
+)
+
+// HandlePanic is a generic panic handler that can restart a long-running
+// task if necessary. Call it like:
+// defer HandlePanic(nil)
+// defer HandlePanic(this.method)
+func HandlePanic(restartable func()) {
+	if r := recover(); r != nil {
+		log.Printf("Panic encountered: %v\n%s", r, debug.Stack())
+		if restartable != nil {
+			time.Sleep(time.Second)
+			go restartable()
+		}
+	}
+}
