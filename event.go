a092d57762e191461b97d2e323d2607b5955b1ed
Shivaram Lingamneni <slingamn@cs.stanford.edu>
2022-05-06 13:02:12 -0400

    add missing Event APIs

--- event.go
+++ event.go
@@ -3,16 +3,47 @@
 
 package godgets
 
+import "time"
+
+// Python's threading.Event with some of the APIs removed:
+// https://docs.python.org/3/library/threading.html#event-objects
+// in particular, this Event can only be used once, so there is no
+// equivalent of clear() to reset the event.
+
 type Event (chan struct{})
 
 func NewEvent() Event {
 	return make(chan struct{})
 }
 
+// Mark the event as completed.
 func (e Event) Done() {
 	close(e)
 }
 
-func (e Event) Wait() {
-	<-e
+// Wait for the event to be completed. A timeout of 0 means no timeout;
+// use IsDone() for a non-blocking check, comparable to Python's is_set().
+func (e Event) Wait(timeout time.Duration) (isDone bool) {
+	if timeout == 0 {
+		<-e
+		return true
+	} else {
+		timer := time.NewTimer(timeout)
+		select {
+		case <-e:
+			isDone = true
+		case <-timer.C:
+		}
+		timer.Stop()
+		return
+	}
+}
+
+func (e Event) IsDone() bool {
+	select {
+	case <-e:
+		return true
+	default:
+		return false
+	}
 }
