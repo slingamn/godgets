695963804afd319be9c28f85c2b8658ebf9b4590
Shivaram Lingamneni <slingamn@cs.stanford.edu>
2023-08-15 20:57:02 -0400

    remove generic utils made obsolete by go1.21
    These now have official counterparts in the `maps` and `slices` packages

--- types.go
+++ types.go
@@ -19,33 +19,3 @@ func (s HashSet[T]) Add(elem T) {
 func (s HashSet[T]) Remove(elem T) {
 	delete(s, elem)
 }
-
-func CopyMap[K comparable, V any](input map[K]V) (result map[K]V) {
-	result = make(map[K]V, len(input))
-	for key, value := range input {
-		result[key] = value
-	}
-	return
-}
-
-func CopySlice[T any](slice []T) (result []T) {
-	result = make([]T, len(slice))
-	copy(result, slice)
-	return
-}
-
-func SliceContains[T comparable](slice []T, elem T) (result bool) {
-	for _, t := range slice {
-		if elem == t {
-			return true
-		}
-	}
-	return false
-}
-
-// reverse the order of a slice in place
-func ReverseSlice[T any](slice []T) {
-	for i, j := 0, len(slice)-1; i < j; i, j = i+1, j-1 {
-		slice[i], slice[j] = slice[j], slice[i]
-	}
-}
